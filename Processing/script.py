3f6570a1273fea329337beb7d1de9c86c48f4876
hamid <hamidabdellaoui55@gmail.com>
2022-06-06 15:38:50 +0100

    up

--- Processing/script.py
+++ Processing/script.py
@@ -4,14 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 np.set_printoptions(precision=2, linewidth=80)
-from nltk import FreqDist
+# from nltk import FreqDist
 # Gensim
 import gensim
 from gensim import corpora
 from gensim.models.coherencemodel import CoherenceModel
 
 import spacy
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import re
 #from bs4 import BeautifulSoup
@@ -19,7 +19,7 @@ import unicodedata
 
 from spacy.lang.fr.stop_words import STOP_WORDS
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 spacy.load('fr_core_news_md')
 import fr_core_news_md #import spacy french stemmer
