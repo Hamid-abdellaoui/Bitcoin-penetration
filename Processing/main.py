b567d266514b7806aaa69762891311f49f89bd06
saad167 <babacheikhsaad@gmail.com>
2022-06-06 12:32:17 +0100

    adding gitignore file

--- Processing/main.py
+++ Processing/main.py
@@ -1,1 +1,35 @@
-print("Helloa")
+################ Import ##################################
+#importation des bibiotheque necessaire
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+np.set_printoptions(precision=2, linewidth=80)
+from nltk import FreqDist
+# Gensim
+import gensim
+from gensim import corpora
+from gensim.models.coherencemodel import CoherenceModel
+
+import spacy
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
+#from bs4 import BeautifulSoup
+import unicodedata
+
+from spacy.lang.fr.stop_words import STOP_WORDS
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+spacy.load('fr_core_news_md')
+import fr_core_news_md #import spacy french stemmer
+from sklearn.decomposition import NMF,LatentDirichletAllocation
+
+import pyLDAvis #Nous utilisons pyLDAvis pour créer des visualisations interactives de modèles de sujet.
+import pyLDAvis.sklearn
+
+#dealing with date
+import dateparser
+
+import datetime
