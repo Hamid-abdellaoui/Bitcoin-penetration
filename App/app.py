6cf6cb70173f00c1ae3524d6c08d33336f9cd390
hamid <hamidabdellaoui55@gmail.com>
2022-06-08 16:03:43 +0100

    up

--- App/app.py
+++ App/app.py
@@ -2,6 +2,12 @@
 from flask import current_app, send_file, send_from_directory
 import sys 
 import os
+import plotly
+import plotly.express as px
+import json
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
 
 from flask import Flask, render_template,request 
 from datetime import datetime
@@ -18,7 +24,25 @@ import numpy as np
 
 k=0
 
+def create_plot():
 
+
+    N = 40
+    x = np.linspace(0, 1, N)
+    y = np.random.randn(N)
+    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+
+
+    data = [
+        go.Bar(
+            x=df['x'], # assign x as the dataframe column 'x'
+            y=df['y']
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
 
 ## to get current year
 @app.context_processor
@@ -27,7 +51,8 @@ def inject_now():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    bar = create_plot()
+    return render_template("index.html", graphJSON=bar)
         
 
 @app.route("/UnderstandML")
